e77dcffd6957ff89d17b408f7c19569e799867c0
learnweekend <learnweekend@gmail.com>
2017-06-10 22:12:17 -0700

    Add files via upload

--- BinaryGap.java
+++ BinaryGap.java
@@ -1,27 +1,20 @@
 /*
 Problem : BinaryGap
-A binary gap within a positive integer N is any maximal sequence of consecutive
-zeros that is surrounded by ones at both ends in the binary representation of N.
-For example, number 9 has binary representation 1001 and contains a binary gap of
-length 2. The number 529 has binary representation 1000010001 and contains two
-binary gaps: one of length 4 and one of length 3. The number 20 has binary r
-epresentation 10100 and contains one binary gap of length 1. The number 15 has
-binary representation 1111 and has no binary gaps.
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones
+at both ends in the binary representation of N. For example, number 9 has binary representation 1001 and contains
+a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps:
+one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary
+gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
 */
 public class BinaryGap {
   public static void main(String args[]) {
-    int number = 529;
-    //int number = 1041;
-    //int number = 9;
-    //int number = 20;
+    int number = 529; //1041; //9; 20;
     int maxBinaryGap = getMaxBinaryGap(number);
     System.out.println("Maximum binary gap of number " + number + " (" +Integer.toBinaryString(number) +") = " +maxBinaryGap);
   }
 /*
-Solution 1: Do a bitwise & with 1 and check if the result is 0 or 1
-            right shift the number to get next bit
-Runtime : O(Log N) -> number of bits
-Space : O(1)
+Solution 1: Do a bitwise & with 1 and check if the result is 0 or 1. Right shift the number to get next bit
+Runtime : O(Log N) -> number of bits, Space : O(1)
 */
   private static int getMaxBinaryGap(int number) {
     if(number <= 0)
