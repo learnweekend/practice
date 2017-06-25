d40b769d2c5b64dbd14bf012a4a61ab06b6fcf6a
learnweekend <learnweekend@gmail.com>
2017-06-25 15:10:06 -0700

    Add files via upload

---
+++ MajorityElement.java
@@ -0,0 +1,35 @@
+/*
+ Problem : Majority Element
+ Given an array of size n, find the majority element.
+ The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ You may assume that the array is non-empty and the majority element always exist in the array.
+ */
+ public class MajorityElement {
+  public static void main(String[] aargs) {
+    //int[] a = {3, 2, 2, 1, 2, 2, 1};
+    int[] a = {3, 1,1, 2, 2, 1, 2, 2, 1, 1, 1, 2};
+    int majorityElement = findMajorityElement(a);
+    System.out.println("majorityElement = " + majorityElement);
+  }
+  /* Solution : Loop through the array and count the number of elements
+    Runetime : O(N), Space : O(1)
+  */
+  private static int findMajorityElement(int[] arr) {
+    if(arr == null || arr.length == 0)
+      throw new IllegalArgumentException();
+    int majorityElement = arr[0];
+    int count = 1;
+
+    for(int i = 1; i < arr.length; i++){
+      if(arr[i] == majorityElement)
+        count++;  // increment
+      else
+        count --;
+      if(count == 0) {
+        majorityElement = arr[i]; //update the majorityElement
+        count++; // update count.
+      }
+    }
+    return majorityElement;
+  }
+ }
