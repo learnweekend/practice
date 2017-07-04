94cded5b9d04257d7b60674235b39bf4c7d5d709
learnweekend <learnweekend@gmail.com>
2017-07-03 23:49:50 -0700

    Add files via upload

--- ParenthesesMatch.java
+++ ParenthesesMatch.java
@@ -1,68 +1,69 @@
+import java.util.*;
 /**
- Given input string as brackets - ()() only.
- Output : Return true if it is valid parenthesis or false
-*/
-import java.util.Stack;
-public class ParenthesesMatch {
-  public static void main(String args[]) {
-    String parenthesis = "()()";
-    boolean isValid1 = validateparenthesis(parenthesis);
-    boolean isValid2 = validateparenthesisV2(parenthesis);
-    System.out.println(isValid1);
-    System.out.println(isValid2);
-  }
-  // Solution 1 : Have one counter for each open parenthesis (+1 for open and -1 for close);
-  // total sum should be 0 for valid and non-zero for invalid
-  // Runtime : O(N)
-  // Space   : O(1)
-  private static boolean validateparenthesisV2(String theStr) {
-    if(theStr == null || theStr.length() == 0)
-        throw new IllegalArgumentException();
-    int count = 0;
+ * Given a string expression contains brackets (), {} or [],
+ * Return true if the expression is valid else false;
+ */
+ public class ParenthesesMatch {
+	  public static void main(String args[]) {
+		    //String str = "{{}}[](){[()]}([])";
+        //String str = "{()[{({})[]()}]}([])";
+        String str = "()()";
+		    System.out.println(isBalancedV1(str));
+		    System.out.println(isBalancedV2(str));
+		    //System.out.println(isBalancedV3(str));
+	  }
+	/* Solution 1: Store the valid parenthese in Map
+	 * Loop through the input and push to stack if the characer is present in the map
+	 * else, pop the stack and check the map value of the character matches.
+	 * return false if doesn't match.
+	 * Runtime : O(N);
+	 * Space   : O(N); */
+	public static boolean isBalancedV1(String expression) {
+		if (expression == null || expression.length() == 0)
+			throw new IllegalArgumentException();
 
-    for(int i = 0; i < theStr.length(); i++) {
-      char ch = theStr.charAt(i);
+		if (expression.length() % 2 != 0) // if valid % 2 == 0
+			return false;
 
-      if(ch == '(' || ch == ')')
-        count += getCharValue(ch);
-      else
-        return false;  // invalid input characters other than '(' or ')'
-    }
-      return (count == 0 ? true : false);
-  }
-  // helper method assign value to open and close parenthesis
-  private static int getCharValue(char c) {
-    if(c == '(')
-      return 1;
-    if(c == ')')
-      return -1;
+		Map<Character, Character> parenthesesMap = new HashMap<>();
+			parenthesesMap.put('(', ')');
+			parenthesesMap.put('{', '}');
+			parenthesesMap.put('[', ']');
+		Stack<Character> stack = new Stack<>();
 
-    return 0;
-  }
-  // Solution 2 : Have additional memory (stack) to store open parenthesis
-  // pop from stack when we see a close parenthesis
-  // make sure the stack is empty for valid parenthesis.
-  // Runtime : O(N)
-  // Space   : O(N)
-  private static boolean validateparenthesis(String theStr) {
-    if(theStr == null || theStr.length() == 0)
-        throw new IllegalArgumentException();
-    Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < expression.length(); i++) {
+			char ch = expression.charAt(i);
+			if (parenthesesMap.containsKey(ch)) {
+				stack.push(ch);
+			} else if(stack.isEmpty() || parenthesesMap.get(stack.pop()) != ch ){
+				return false;
+			}
+		}
+		return stack.isEmpty();
+	}
+	/* Solution 2: Loop through the input and push to stack if the characer is open parenthesis
+	 * else, pop the stack and check if popped element is matching open parenthesis.
+	 * return false if doesn't match.
+	 * Runtime : O(N);
+	 * Space   : O(N);  */
+	public static boolean isBalancedV2(String expression) {
+		if (expression == null || expression.length() == 0)
+			throw new IllegalArgumentException();
+		if (expression.length() % 2 != 0)
+			return false;
+		Stack<Character> stack = new Stack<>();
 
-    for(int i = 0; i < theStr.length() ; i++) {
-      char c = theStr.charAt(i);
-      if(c == '(') {
-        stack.push(c);
-      } else if(c == ')') {
-        if(stack.isEmpty() || stack.pop() != '(')
-          return false;
-      } else {
-        return false; // invalid input characters other than '(' or ')'
-      }
-    }
-    if(!stack.isEmpty())
-      return false;
-
-    return true;
-  }
+		for (char c : expression.toCharArray()) {
+			if (c == '{' || c == '[' || c == '(') {
+				stack.push(c);
+			} else if (c == '}' && (stack.isEmpty() || stack.pop() != '{')) {
+				return false;
+			} else if (c == ']' && (stack.isEmpty() || stack.pop() != '[')) {
+				return false;
+			} else if (c == ')' && (stack.isEmpty() || stack.pop() != '(')) {
+				return false;
+			}
+		}
+		return stack.isEmpty();
+	}
 }
