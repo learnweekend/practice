61a8a728663df8eea6aa4aaf28756a3381b44521
learnweekend <learnweekend@gmail.com>
2017-07-03 23:29:25 -0700

    Add files via upload

--- SwapWithoutTemp.java
+++ SwapWithoutTemp.java
@@ -1,26 +1,26 @@
 
-// Given two positive integers, swap them without using "temp" variable
+// Given two integers (positive/negative), swap them without using "temp" variable
 public class SwapWithoutTemp {
   public static void main(String args[]) {
-    int a = 10;
-    int b = 20;
+    int a = -10;
+    int b = -20;
     swapV1(a, b);
     swapV2(a, b);
   }
   // Solution 1 : Use XOR to swap integers
   private static void swapV1(int x, int y) {
-    System.out.println("before swap : x = " + x + ", y = " +y);
+    System.out.println("original   : x = " + x + ", y = " +y);
     x = x ^ y;   //1010 ^ 10100  = 11110
     y = x ^ y;
     x = x ^ y;
-    System.out.println("after  swap : x = " + x + ", y = " +y);
+    System.out.println("solution 1 : x = " + x + ", y = " +y);
   }
   // Solution 2 : Use Addition
   private static void swapV2(int x, int y) {
-    System.out.println("before swap : x = " + x + ", y = " +y);
+    //System.out.println("before swap : x = " + x + ", y = " +y);
     x = x + y;
     y = x - y;
     x = x - y;
-    System.out.println("after  swap : x = " + x + ", y = " +y);
+    System.out.println("solution 2 : x = " + x + ", y = " +y);
   }
 }
