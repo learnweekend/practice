4e78d05c7ba4cc16fe3e81118e9d226c3080b6e7
learnweekend <learnweekend@gmail.com>
2017-12-21 22:04:11 -0800

    Update MoveZeros.java

--- MoveZeros.java
+++ MoveZeros.java
@@ -1,47 +1,39 @@
-
 import java.util.Arrays;
 
 public class MoveZeros {
 	
-	/**
-	 * Runtime : O(N), Space : O(1)
-	 */
-	private static void moveZerosToRight(int[] arr){
-    	    int index = 0;
-    	    for(int i = 0; i < arr.length; i++){
-      	       if(arr[i] != 0)
-        	 arr[index++] = arr[i];
-    	    }
-           while(index < arr.length)
-              arr[index++] = 0;
-        }
+	/* Move Zeros to End of the Array, runtime : O(N), Space : O(1) */
 	
-	/**
-	 * Runtime : O(N), Space : O(1)
-	 */
-	private static void moveZerosToLeft(int[] arr) {
-	    int index = arr.length - 1;
-	    int i = arr.length - 1; // track non-zero element
+	private static void moveToEnd(int[] arr){
+		int index = 0;
 		
-	    while(i >= 0) {
-		if(arr[i] != 0) {
-		   swap(arr, i, index);
-		   index--;
-		 }
-		 i--;
-	     }
-	 }
+		for(int i = 0; i < arr.length; i++)
+		     if(arr[i] != 0)
+			arr[index++] = arr[i];
 
-	public static void main(String[] args) {
-		moveZerosToLeft(new int[] {1, 2, 3, 0, 0, 4, 0, 5, 0, 6});
-		moveZerosToLeft(new int[] {1,0});
-		moveZerosToLeft(new int[] {0, 1});
-		moveZerosToLeft(new int[] {0});
+		while(index < arr.length)
+			arr[index++] = 0;
 		
-		moveZerosToRight(new int[] {1, 2, 3, 0, 0, 4, 0, 5, 0, 6});
-		moveZerosToRight(new int[] {1,0});
-		moveZerosToRight(new int[] {0, 1});
-		moveZerosToRight(new int[] {0});
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	/* Move Zeros to Start of the Array, runtime : O(N), Space : O(1) */
+	
+	private static void moveToStart(int[] arr){
+		int index = arr.length - 1;
+		
+		for(int i = arr.length - 1; i >= 0; i--)
+		    if(arr[i] != 0)
+		       arr[index--] = arr[i];
+
+		while(index >= 0)
+		     arr[index--] = 0;
+		
+		System.out.println(Arrays.toString(arr));
 	}
 
+	public static void main(String[] args) {
+	      moveToEnd(new int[] {0, 2, 4, 0, 1, 0, 0, 3, 2});
+	      moveToStart(new int[] {0, 2, 4, 0, 1, 0, 0, 3, 2});
+	}
 }
