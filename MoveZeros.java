9197f42fa41dcb558aa40dd484baacc08384174d
learnweekend <learnweekend@gmail.com>
2017-12-21 21:15:07 -0800

    Update MoveZeros.java

--- MoveZeros.java
+++ MoveZeros.java
@@ -5,55 +5,33 @@ import java.util.Arrays;
 public class MoveZeros {
 	
 	/**
-	 * Solution : Brute force
-	 * 1. Traverse the array from end and place the non-zero elements from end.
-	 * 2. During the traversal count the number of zeros.
-	 * 3. Fill the zeros in the start to match the zeros count.
-	 * Runtime : O(N), Space : O(1)
-	 */
-	private static void moveZerosToLeft(int[] arr){
-		
-		int index = arr.length - 1;
-		int zerosCount = 0;
-		
-		for(int i = arr.length - 1; i >= 0; i--){
-			if(arr[i] != 0){
-				arr[index--] = arr[i];
-			} else {
-				zerosCount++;
-			}
-		}
-		for(int i = 0; i < zerosCount; i++){
-			arr[i] = 0;
-		}
-		System.out.println(Arrays.toString(arr));
-	}
-	
-	/**
-	 * Solution : Brute force
-	 * 1. Traverse the array from start and place the non-zero elements from beginning.
-	 * 2. During the traversal count the number of zeros.
-	 * 3. Fill the zeros in the end to match the zeros count.
 	 * Runtime : O(N), Space : O(1)
 	 */
 	private static void moveZerosToRight(int[] arr){
+    	    int index = 0;
+    	    for(int i = 0; i < arr.length; i++){
+      	       if(arr[i] != 0)
+        	 arr[index++] = arr[i];
+    	    }
+           while(index < arr.length)
+              arr[index++] = 0;
+        }
+	
+	/**
+	 * Runtime : O(N), Space : O(1)
+	 */
+	private static void moveZerosToLeft(int[] arr) {
+	    int index = arr.length - 1;
+	    int i = arr.length - 1; // track non-zero element
 		
-		int index = 0;
-		int zerosCount = 0;
-		
-		for(int i = 0; i < arr.length; i++){
-			if(arr[i] != 0){
-				arr[index++] = arr[i];
-			} else {
-				zerosCount++;
-			}
-		}
-		for(int i = 0; i < zerosCount; i++){
-			arr[index++] = 0;
-		}
-		System.out.println(Arrays.toString(arr));
-	}
-
+	    while(i >= 0) {
+		if(arr[i] != 0) {
+		   swap(arr, i, index);
+		   index--;
+		 }
+		 i--;
+	     }
+	 }
 
 	public static void main(String[] args) {
 		moveZerosToLeft(new int[] {1, 2, 3, 0, 0, 4, 0, 5, 0, 6});
