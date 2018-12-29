7b35003772473e8592553070dc3a2aa3074c9806
learnweekend <learnweekend@gmail.com>
2018-12-28 16:35:39 -0800

    Update PalindromePermutation.java

--- PalindromePermutation.java
+++ PalindromePermutation.java
@@ -1,18 +1,17 @@
-/*
- problem : Palindrome Permutation
- Given a string, determine if a permutation of the string could form a palindrome.
- For example : “code” -> False, “aab” -> True, “carerac” -> True.
- https://leetcode.com/articles/palindrome-permutation/
- */
- import java.util.*;
 
- public class PalindromePermutation {
-  public static void main(String[] args) {
-    String s = "carerac";
-    System.out.println(s + " : is Palindrome Permutation ? : " + isPermutationPalindrome(s));
-  }
-  
-  	// solution 1 : 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/palindrome-permutation/description/ 
+ * Given a string, determine if a permutation of the string could form a palindrome. 
+ * Example 
+ * 1: Input: "code" Output: false Example
+ * 2: Input: "aab" Output: true
+ */
+public class PalindromePermutation {
+
+	// solution 1 : Runtime : O(N) | Space : O(1)
 	public static boolean canPermutePalindrome(String s) {
 		int[] count = new int[128];
 		int uniques = 0;
@@ -27,28 +26,29 @@
 		return uniques <= 1;
 	}
 
-  public static boolean isPermutationPalindrome(String s) {
-    if(s == null) throw new IllegalArgumentException();
-    if(s.length() == 0) return true;
-    Map<Character, Integer> countMap = new HashMap<>();
-    // character -> count map
-    for(int i = 0; i < s.length(); i++) {
-      char ch = s.charAt(i);
-      if(countMap.containsKey(ch)){
-        countMap.put(ch, countMap.get(ch) + 1);
-      } else {
-        countMap.put(ch, 1);
-      }
-    }
-    int oddCharCounts = 0;
-    // traverse the countMap and check for number of odd character counts, if more than 1, return false.
-    for(Character ch : countMap.keySet()){
-      if(countMap.get(ch) % 2 == 1){ //
-        oddCharCounts++;
-      }
-      if(oddCharCounts > 1)
-          return false;
-     }
-     return true;
-   }
- }
+	// solution 2: Runtime : O(N) | Space : O(1)
+	public static boolean isPermutationPalindrome(String str) {
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (map.containsKey(ch)) {
+				map.put(ch, map.get(ch) + 1);
+			} else {
+				map.put(ch, 1);
+			}
+		}
+		int count = 0;
+		for (char ch : map.keySet()) {
+			count += map.get(ch) % 2;
+			if (count > 1)
+				return false;
+		}
+		return count <= 1;
+	}
+
+	public static void main(String[] args) {
+		String str = "aab";
+		System.out.println(isPermutationPalindrome(str));
+		System.out.println(canPermutePalindrome(str));
+	}
+}
