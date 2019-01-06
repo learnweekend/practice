1bc179de945db509c265a424b1c20f2903b49ac8
learnweekend <learnweekend@gmail.com>
2019-01-06 10:57:14 -0800

    Update ArrayProductExceptSelf.java

--- ArrayProductExceptSelf.java
+++ ArrayProductExceptSelf.java
@@ -1,4 +1,6 @@
 /**
+https://leetcode.com/problems/product-of-array-except-self/description/
+
 * Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of
 * all the elements of nums except nums[i].
 * Solve it without division and in O(n).
